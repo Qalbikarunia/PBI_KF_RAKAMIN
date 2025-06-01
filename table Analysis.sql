332da83a59f5321b8c6e0b859185d5dbaf4e5cea
Qalbi Karunia <101724176+Qalbikarunia@users.noreply.github.com>
2025-06-01 17:46:34 +0700

    Update table Analysis.sql

--- table Analysis.sql
+++ table Analysis.sql
@@ -1,41 +1,42 @@
 --Pembuatan Table Analysis
 
 CREATE OR REPLACE TABLE kimia_farma.kf_analysis AS
-SELECT 
-  ft.transaction_id,
-  ft.date,
-  kc.branch_id,
-  kc.branch_name,
-  kc.kota,
-  kc.provinsi,
-  kc.rating as rating_cabang,
-  ft.customer_name,
-  p.product_id,
-  p.product_name,
-  p.price as actual_price,
-  ft.discount_percentage,
-  CASE
-    WHEN p.price <= 50000 THEN 0.1
-    WHEN p.price BETWEEN 50000 AND 100000 THEN 0.15
-    WHEN p.price BETWEEN 100000 AND 300000 THEN 0.2
-    WHEN p.price BETWEEN 300000 AND 500000 THEN 0.25
-    WHEN p.price > 500000 THEN 0.3
-  END AS percentage_gross_laba,
-  (p.price * (1 - ft.discount_percentage)) as nett_sales,
-  (((p.price * (1 - ft.discount_percentage)) * 
-  CASE
-    WHEN p.price <= 50000 THEN 0.1
-    WHEN p.price BETWEEN 50000 AND 100000 THEN 0.15
-    WHEN p.price BETWEEN 100000 AND 300000 THEN 0.2
-    WHEN p.price BETWEEN 300000 AND 500000 THEN 0.25
-    WHEN p.price > 500000 THEN 0.3
-  END) - ((p.price * (1 - ft.discount_percentage)) - p.price)) as nett_profit,
-  ft.rating as rating_transaksi
-FROM `rakamin-kf-analytics-460916.kimia_farma.kf_final_transaction` as ft
-  INNER JOIN `rakamin-kf-analytics-460916.kimia_farma.kf_kantor_cabang`as kc
+WITH base_data AS (
+  SELECT 
+    ft.transaction_id,
+    ft.date,
+    kc.branch_id,
+    kc.branch_name,
+    kc.kota,
+    kc.provinsi,
+    kc.rating AS rating_cabang,
+    ft.customer_name,
+    p.product_id,
+    p.product_name,
+    p.price AS actual_price,
+    ft.discount_percentage,
+    CASE
+      WHEN p.price <= 50000 THEN 0.1
+      WHEN p.price BETWEEN 50000 AND 100000 THEN 0.15
+      WHEN p.price BETWEEN 100000 AND 300000 THEN 0.2
+      WHEN p.price BETWEEN 300000 AND 500000 THEN 0.25
+      WHEN p.price > 500000 THEN 0.3
+    END AS percentage_gross_laba,
+    ft.rating as rating_transaksi
+  FROM `rakamin-kf-analytics-460916.kimia_farma.kf_final_transaction` as ft
+  LEFT JOIN `rakamin-kf-analytics-460916.kimia_farma.kf_kantor_cabang`as kc
     ON ft.branch_id = kc.branch_id
-  INNER JOIN `rakamin-kf-analytics-460916.kimia_farma.kf_product` as p
+  LEFT JOIN `rakamin-kf-analytics-460916.kimia_farma.kf_product` as p
     ON ft.product_id = p.product_id
+)
+SELECT *,
+  (actual_price * (1 - discount_percentage)) AS nett_sales,
+  (
+    (actual_price * (1 - discount_percentage)) * percentage_gross_laba
+    - ((actual_price * (1 - discount_percentage)) - actual_price)
+  ) AS nett_profit
+FROM base_data;
+
 
 -- Panggil table
 SELECT * 
